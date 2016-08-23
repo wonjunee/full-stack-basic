bde23d5dcc9b9fd2488b9db5fcadf6d6c4ce9000
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-08-22 22:35:12 -0400

    added def

--- mindstorms.py
+++ mindstorms.py
@@ -1,5 +1,10 @@
 import turtle
 
+def draw_square(some_turtle):
+	for i in rannge(1,5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
 def draw_square():
 	window = turtle.Screen()
 	window.bgcolor("red")
@@ -8,10 +13,7 @@ def draw_square():
 	brad.shape("turtle")
 	brad.color("yellow")
 	brad.speed(2)
-
-	for i in range(4):
-		brad.forward(100)
-		brad.right(90)
+	draw_square(brad)
 
 	angie = turtle.Turtle()
 	angie.shape("arrow")
