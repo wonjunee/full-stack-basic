654c2bb9319e9531a3918a6f21e20e07a43b93ca
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-08-22 22:33:06 -0400

    added new tuttle angie

--- mindstorms.py
+++ mindstorms.py
@@ -8,10 +8,15 @@ def draw_square():
 	brad.shape("turtle")
 	brad.color("yellow")
 	brad.speed(2)
-	
+
 	for i in range(4):
 		brad.forward(100)
 		brad.right(90)
+
+	angie = turtle.Turtle()
+	angie.shape("arrow")
+	angie.color("blue")
+	angie.circle(100)
 
 	window.exitonclick()
 
