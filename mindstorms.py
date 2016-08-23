7fdf4e69a304f007e18344d0abeb78d1efb80f99
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-08-22 22:32:16 -0400

    added color and shape

--- mindstorms.py
+++ mindstorms.py
@@ -5,6 +5,10 @@ def draw_square():
 	window.bgcolor("red")
 
 	brad = turtle.Turtle()
+	brad.shape("turtle")
+	brad.color("yellow")
+	brad.speed(2)
+	
 	for i in range(4):
 		brad.forward(100)
 		brad.right(90)
