2d59e7f6e7d7b05d2d6c4c9487c1f9d07b419490
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-08-22 22:15:52 -0400

    added rename and translate functions

--- rename.py
+++ rename.py
@@ -5,4 +5,7 @@ def rename_files():
 	file_list = os.listdir(r"./prank")
 	print(file_list)
 
+	# for each file, rename filename
+	for file_name in file_list:
+		os.rename(file_name, file_name.translate(None, "0123456789"))
 rename_files()
