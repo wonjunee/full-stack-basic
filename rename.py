d3947d79c1377652c0f4826fa194f8bea875b5ed
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-08-22 22:19:27 -0400

    modified

--- rename.py
+++ rename.py
@@ -3,9 +3,17 @@ import os
 def rename_files():
 	# get file names from a folder
 	file_list = os.listdir(r"./prank")
-	print(file_list)
 
+	# print path
+	saved_path = os.getcwd()
+	print "current path:{}".format(saved_path)
+	# Change the path
+	os.chdir(r"./prank")
+	saved_path = os.getcwd()
+	print "current path:{}".format(saved_path)
 	# for each file, rename filename
 	for file_name in file_list:
+		print("Old Name - "+ file_name)
+		print("Old Name - "+ file_name.translate(None, "0123456789"))
 		os.rename(file_name, file_name.translate(None, "0123456789"))
 rename_files()
